26ef61971644af186a4acb4751e5c32030867901
estudiante <estudiante@ISIS2603-327.sis.virtual.uniandes.edu.co>
2018-10-20 14:24:16 -0500

    Modulo de producto

---
+++ src/app/producto/producto.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoListComponent } from './producto-list/producto-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProductoListComponent]
+})
+export class ProductoModule { }
