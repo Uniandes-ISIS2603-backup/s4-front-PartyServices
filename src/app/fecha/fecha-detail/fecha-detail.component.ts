12ee9e2fe4d2d687c6c952949e702ef98700ed67
estudiante <estudiante@ISIS2603-337.sis.virtual.uniandes.edu.co>
2018-10-23 15:26:45 -0500

    Fecha detail OK

--- src/app/fecha/fecha-detail/fecha-detail.component.ts
+++ src/app/fecha/fecha-detail/fecha-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Fecha } from '../fecha';
+import { FechaService } from '../fecha.service';
 
 @Component({
   selector: 'app-fecha-detail',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FechaDetailComponent implements OnInit {
 
-  constructor() { }
+  @Input() fecha : Fecha;
+
+  constructor(private fechaService : FechaService) { }
 
   ngOnInit() {
+    this.getFecha();
+  }
+
+  getFecha(){
+    this.fechaService.getFecha(51,"2018-11-10T00:00:00-05:00","Tarde")
+      .subscribe( fecha=>{
+        this.fecha=fecha;
+      }
+    );
   }
 
 }
