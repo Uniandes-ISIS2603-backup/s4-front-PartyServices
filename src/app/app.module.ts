709ac79283d488c6991b548d869373c863203aaf
estudiante <estudiante@ISIS2603-326.sis.virtual.uniandes.edu.co>
2018-10-24 21:42:47 -0500

    Se solucionó el error que no dejaba instalar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,30 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HttpErrorInterceptor } from './interceptors/httperrorinterceptor.service';
+import { ProductoModule } from './producto/producto.module';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClienteModule } from './cliente/cliente.module';
+
+import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 import { AgendaModule } from './agenda/agenda.module';
 import { FechaModule } from './fecha/fecha.module';
-import { ClienteModule } from './cliente/cliente.module';
-import { ProductoModule } from './producto/producto.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
     ProductoModule,
     ClienteModule,
+    HttpClientModule,
     AgendaModule,
     FechaModule,
-    
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
-  bootstrap: [AppComponent],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: HttpErrorInterceptor,
-            multi: true
-        }
-    ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cliente/cliente.service.ts
+++ src/app/cliente/cliente.service.ts
@@ -3,13 +3,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Cliente } from './cliente';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 /**
 * El servicio para proveer todo lo relacionado con cliente.
 */
-const API_URL = environment.apiURL;
-const clientes = '/clientes';
+const API_URL = "../../assets/";
+const clientes = 'clientes.json';
 
 @Injectable()
 export class ClienteService {
