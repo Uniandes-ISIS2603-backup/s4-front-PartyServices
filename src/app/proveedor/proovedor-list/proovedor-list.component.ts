ed634131424acb2f4d34d40ddb8fcadfa53f86a0
estudiante <estudiante@ISIS2603-380.sis.virtual.uniandes.edu.co>
2018-10-25 11:07:27 -0500

    proveedor list creado

---
+++ src/app/proveedor/proovedor-list/proovedor-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-proovedor-list',
+  templateUrl: './proovedor-list.component.html',
+  styleUrls: ['./proovedor-list.component.css']
+})
+export class ProovedorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/proveedor/proveedor.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProovedorListComponent } from '../Proveedor/proovedor-list/proovedor-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ProovedorListComponent]
+})
+export class ProveedorModule { }
