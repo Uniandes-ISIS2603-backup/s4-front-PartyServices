c50c91dd74e54aa9573e4ee876aac69ace82bd2c
estudiante <estudiante@ISIS2603-326.sis.virtual.uniandes.edu.co>
2018-10-25 04:53:29 -0500

    Haber si ya logro poner todo bien

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,15 +1,24 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from '../app-routing/app-routing.module';
+
 import { ClienteListComponent } from './cliente-list/cliente-list.component';
 import { ClienteService } from './cliente.service';
 
 @NgModule({
   imports: [
-    CommonModule
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        CommonModule,
+        FormsModule
   ],
   declarations: [ClienteListComponent],
   providers: [ClienteService],
-  exports:[ClienteListComponent]
+  exports:[ClienteListComponent],
 })
 export class ClienteModule { }
