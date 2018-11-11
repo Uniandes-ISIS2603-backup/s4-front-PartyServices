9fd6b82b6b412ebb884b8fd6f2f5ea67e69be27d
estudiante <estudiante@ISIS2603-327.sis.virtual.uniandes.edu.co>
2018-11-10 22:53:02 -0500

    todo comentado

--- src/app/producto/producto-detail/producto-detail.component.ts
+++ src/app/producto/producto-detail/producto-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import{ProductoService } from '../../producto/producto.service';
+import { ProductoService } from '../../producto/producto.service';
 import { ActivatedRoute } from '@angular/router';
-import{Producto} from '../../producto/producto' ;
+import { Producto } from '../../producto/producto';
 @Component({
   selector: 'app-producto-detail',
   templateUrl: './producto-detail.component.html',
@@ -9,33 +9,48 @@ import{Producto} from '../../producto/producto' ;
 })
 export class ProductoDetailComponent implements OnInit {
 
-
+/**
+ * Constructor del componente
+ * @param productoService
+ * @param route 
+ */
   constructor(
     private productoService: ProductoService,
-    private route: ActivatedRoute 
+    private route: ActivatedRoute
 
   ) { }
 
+  /**
+   * Objeto que representa a un producto
+   */
   producto: Producto;
 
-  producto_nombre: string ;
-    
+  /**
+   * Variable del nombre con el cual se va a buscar en la lista de productos
+   */
+  producto_nombre: string;
 
-  ngOnInit() 
-  {
+  /**
+    * Esto inicializará el componente recuperando el detail del producto del servicio
+    * Este método se llamará justo cuando el componente sea creado. 
+    */
+  ngOnInit() {
 
-    this.producto_nombre = this.route.snapshot.paramMap.get('nombre') ;
-    this.producto = new Producto() ;
-    this.getProductoDetail() ;
+    this.producto_nombre = this.route.snapshot.paramMap.get('nombre');
+    this.producto = new Producto();
+    this.getProductoDetail();
   }
 
+  /**
+   * Llama al servicio para obtener el producto del cual se mostrará la información
+   */
   getProductoDetail(): void {
     this.productoService.getProductoDetail(this.producto_nombre)
-       .subscribe(producto => {
-           this.producto = producto;
-       });
-}
- 
- 
+      .subscribe(producto => {
+        this.producto = producto;
+      });
+  }
+
+
 
 }
--- src/app/producto/producto.module.ts
+++ src/app/producto/producto.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductoListComponent } from './producto-list/producto-list.component';
-import { ProductoService} from './producto.service';
-import { ProductoDetailComponent } from './producto-detail/producto-detail.component' ;
-import {AppRoutingModule} from '../app-routing/app-routing.module';
+import { ProductoService } from './producto.service';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
+import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { ProductoCreateComponent } from './producto-create/producto-create.component';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
+/**
+ * Clase typeScript del modulo Producto
+ */
 
 @NgModule({
   imports: [
@@ -27,9 +29,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     ProductoDetailComponent,
     ProductoCreateComponent
   ],
-  providers: [ProductoService] ,
+  providers: [ProductoService],
   exports: [
     ProductoListComponent, ProductoDetailComponent, ProductoCreateComponent
-  ] 
+  ]
 })
 export class ProductoModule { }
