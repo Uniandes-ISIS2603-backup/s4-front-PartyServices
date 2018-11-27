988b2399f9a858cd41216dac7b19c11f47fa79e8
estudiante <estudiante@ISIS2603-380.sis.virtual.uniandes.edu.co>
2018-11-27 17:03:08 -0500

    cosas

--- src/app/proveedor/proveedor-detail/proveedor-detail.component.ts
+++ src/app/proveedor/proveedor-detail/proveedor-detail.component.ts
@@ -1,11 +1,11 @@
 
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-
+import { ToastrService } from 'ngx-toastr';
 import {ProveedorService} from '../proveedor.service';
 import {Proveedor} from '../proveedor';
 import {ProveedorDetail} from '../proveedor-detail';
-import {Producto} '../producto';
+import { Producto } from '../../producto/producto';
 
 @Component({
   selector: 'app-proveedor-detail',
@@ -22,7 +22,8 @@ export class ProveedorDetailComponent implements OnInit {
 */
   constructor(
     private route: ActivatedRoute,
-    private proveedorService:ProveedorService
+    private proveedorService:ProveedorService,
+    private toastrService: ToastrService
   ) { }
   
   /**
@@ -61,8 +62,6 @@ export class ProveedorDetailComponent implements OnInit {
     }
     
     compare(string1: string, string2:string): boolean{
-        console.log(string2);
-        console.log(string1);
         if(string1 === string2)
         {
             return true;
@@ -103,11 +102,9 @@ showHideEdit(proveedor_Id): void {
 
   actualizarProveedor(): void {
     this.showEdit=!this.showEdit;
-    this.ngOnInit();
   }
 
   cancelarEdicion():void {
-    this.showHideEdit();
     this.proveedorDetail= new Proveedor();
     this.getProveedorDetail();
   }
